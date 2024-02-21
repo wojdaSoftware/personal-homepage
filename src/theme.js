53f65783549178c8f74bf17539aa8f079a2e65d9
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-21 18:23:42 +0100

    more colors added

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,11 @@ export const theme = {
   breakpoint: 767,
   color: {
     whiteLilac: "#FBFBFE",
-    slateGray: "#6E7E91"
+    slateGray: "#6E7E91",
+    white: "#FFFFFF",
+    scienceBlue: "#0366D6",
+    iron: "rgba(209, 213, 218, 0.3)",
+    parsley: "rgba(20, 70, 32, 0.2)",
+    anakiwa: "#8CC2FF",
   },
 }
