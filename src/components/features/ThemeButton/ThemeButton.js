1eee381afdb82d96a525230f8aac0520f6ce5db5
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-23 15:25:56 +0100

    theme button changes global state

--- src/components/features/ThemeButton/ThemeButton.js
+++ src/components/features/ThemeButton/ThemeButton.js
@@ -1,12 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
 import { ThemeButtonContainer, StyledThemeButtonText, StyledThemeButton } from "./styled";
+import { selectTheme, toggleTheme } from "./themeButtonSlice";
 
-const ThemeButton = () => (
-  <ThemeButtonContainer>
-    <StyledThemeButtonText>
-      DARK MODE OFF
-    </StyledThemeButtonText>
-    <StyledThemeButton />
-  </ThemeButtonContainer>
-);
+const ThemeButton = () => {
+  const dispatch = useDispatch();
+  const darkTheme = useSelector(selectTheme);
+
+  return (
+    <ThemeButtonContainer>
+      <StyledThemeButtonText>
+        DARK MODE {darkTheme ? "ON" : "OFF"}
+      </StyledThemeButtonText>
+      <StyledThemeButton onClick={() => dispatch(toggleTheme())} />
+    </ThemeButtonContainer>
+  );
+}
 
 export default ThemeButton;
--- src/components/features/ThemeButton/themeButtonSlice.js
+++ src/components/features/ThemeButton/themeButtonSlice.js
@@ -6,14 +6,16 @@ const themeButtonSlice = createSlice({
     darkTheme: false
   },
   reducers: {
-    toggleTheme: ({ darkTheme }) => {
-      darkTheme = !darkTheme;
+    toggleTheme: (state) => {
+      state.darkTheme = !state.darkTheme;
     }
   }
 })
 
+const selectThemeState = state => state.themeButton;
+
 export const { toggleTheme } = themeButtonSlice.actions;
 
-export const selectTheme = state => state.darkTheme;
+export const selectTheme = state => selectThemeState(state).darkTheme;
 
 export default themeButtonSlice.reducer;
