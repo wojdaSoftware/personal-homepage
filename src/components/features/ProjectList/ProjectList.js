aae3b58bbbfcfff47cc58e2307e035bc863d2b86
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-23 16:27:42 +0100

    created a project list item component

--- src/components/features/ProjectList/ProjectList.js
+++ src/components/features/ProjectList/ProjectList.js
@@ -1,27 +1,11 @@
-import { StyledList, StyledListItem, StyledProjectDescription, StyledProjectTitle } from "./styled";
+import ProjectListItem from "./ProjectListItem";
+import { StyledList } from "./styled";
 
 const ProjectList = () => (
   <StyledList>
-    <StyledListItem>
-      <StyledProjectTitle>Lorem Ipsum</StyledProjectTitle>
-      <StyledProjectDescription>Lorem Ipsum Dolor Sit Amet</StyledProjectDescription>
-      <StyledProjectDescription>Dolor Sit Amet</StyledProjectDescription>
-    </StyledListItem>
-    <StyledListItem>
-      <StyledProjectTitle>Lorem Ipsum</StyledProjectTitle>
-      <StyledProjectDescription>Lorem Ipsum Dolor Sit Amet</StyledProjectDescription>
-      <StyledProjectDescription>Dolor Sit Amet</StyledProjectDescription>
-    </StyledListItem>
-    <StyledListItem>
-      <StyledProjectTitle>Lorem Ipsum</StyledProjectTitle>
-      <StyledProjectDescription>Lorem Ipsum Dolor Sit Amet</StyledProjectDescription>
-      <StyledProjectDescription>Dolor Sit Amet</StyledProjectDescription>
-    </StyledListItem>
-    <StyledListItem>
-      <StyledProjectTitle>Lorem Ipsum</StyledProjectTitle>
-      <StyledProjectDescription>Lorem Ipsum Dolor Sit Amet</StyledProjectDescription>
-      <StyledProjectDescription>Dolor Sit Amet</StyledProjectDescription>
-    </StyledListItem>
+    <ProjectListItem />
+    <ProjectListItem />
+    <ProjectListItem />
   </StyledList>
 );
 
---
+++ src/components/features/ProjectList/ProjectListItem/index.js
@@ -0,0 +1,13 @@
+import {StyledListItem, StyledProjectDescription, StyledProjectTitle } from "./styled";
+
+const ProjectListItem = () => {
+  return (
+    <StyledListItem>
+      <StyledProjectTitle>Lorem Ipsum</StyledProjectTitle>
+      <StyledProjectDescription>Lorem Ipsum Dolor Sit Amet</StyledProjectDescription>
+      <StyledProjectDescription>Dolor Sit Amet</StyledProjectDescription>
+    </StyledListItem>
+  );
+};
+
+export default ProjectListItem;
