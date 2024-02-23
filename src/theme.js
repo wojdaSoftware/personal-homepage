f6386698024562601273119858ddabe58441940d
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-23 15:37:44 +0100

    changed color name

--- src/theme.js
+++ src/theme.js
@@ -11,7 +11,7 @@ export const theme = {
     black: "#252525",
     scienceBlue: "#0366D6",
     scienceBlueTransparent: "rgba(3, 102, 214, 0.2)",
-    iron: "rgba(209, 213, 218, 0.3)",
+    ironTransparent: "rgba(209, 213, 218, 0.3)",
     parsley: "rgba(20, 70, 32, 0.2)",
     anakiwa: "#8CC2FF",
     violet: "rgba(9, 10, 51, 0.03)",
