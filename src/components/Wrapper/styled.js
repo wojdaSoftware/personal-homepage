f4ef7070919a12b1e03f26bd6d667bdaf0411c35
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-26 11:49:42 +0100

    added transition time

--- src/components/Wrapper/styled.js
+++ src/components/Wrapper/styled.js
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 export const StyledWrapper = styled.div`
   background-color: ${({ theme }) => theme.color.whiteLilac};
   padding: 119px 14px 0;
+  transition-duration: 200ms;
 
   ${({ $darkTheme }) => $darkTheme && css`
     background-color: ${({ theme }) => theme.color.black};
