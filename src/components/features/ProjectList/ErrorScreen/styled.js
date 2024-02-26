8a9a9707da63c838a09ec81a7ae8e4ee9b30a829
Kacper Wojda <wojda.kacper@gmail.com>
2024-02-26 15:53:04 +0100

    mobile and dark theme styling (Error Screen finished)

--- src/components/features/ProjectList/ErrorScreen/styled.js
+++ src/components/features/ProjectList/ErrorScreen/styled.js
@@ -1,9 +1,19 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ReactComponent as DangerIcon } from '../../../../images/danger-icon.svg'
 
 export const StyledDangerIcon = styled(DangerIcon)`
   margin-top: 88px;
   color: ${({ theme }) => theme.color.black};
+
+  ${({ $darkTheme }) => $darkTheme && css`
+    color: ${({ theme }) => theme.color.white};
+  `}
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+      width: 32px;
+      height: 32px;
+      margin-top: 36px;
+    }
 `;
 
 export const StyledDisclaimer = styled.strong`
@@ -13,6 +23,14 @@ export const StyledDisclaimer = styled.strong`
   margin-top: 16px;
   display: block;
   color: ${({ theme }) => theme.color.black};
+
+  ${({ $darkTheme }) => $darkTheme && css`
+    color: ${({ theme }) => theme.color.white};
+  `}
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+      font-size: 16px;
+    }
 `;
 
 export const StyledDescription = styled.p`
@@ -21,4 +39,14 @@ export const StyledDescription = styled.p`
   letter-spacing: 1px;
   margin: 32px 0 0 0;
   color: ${({ theme }) => theme.color.black};
+
+  ${({ $darkTheme }) => $darkTheme && css`
+    color: ${({ theme }) => theme.color.white};
+  `}
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+      font-size: 14px;
+      line-height: 20px;
+      margin-top: 16px;
+    }
 `;
